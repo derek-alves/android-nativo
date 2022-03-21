f8e2d682a9991cbf5c0ec91fb6aaa61163903044
derek-alves <derek.alves1998@hotmail.com>
2022-03-21 19:42:10 -0300

    step: study string interpolation

---
+++ kotlin/1/src/string_templates.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val nome:String = "Derek"
+    println("Hello $nome")
+
+
+    println("soma: ${soma(10,20)}")
+
+}
+
+fun soma(a:Int, b:Int): Int{
+    return a + b
+}
---
+++ kotlin/1/src/variables.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val nome:String = "Derek"
+    var nome1: String = "Derek"
+
+
+    //nome = "oi"
+    nome1 = "oi"
+
+    println(nome + nome1)
+}
