cf101dcecd110136b491bc61b5f4979077ef21bb
derek-alves <derek.alves1998@hotmail.com>
2022-03-27 20:18:07 -0300

    step: add file and configuration equal to .env

--- curso-android-marvel-app/core/src/main/java/com/example/core/data/network/response/DataContainerResponse.kt
+++ curso-android-marvel-app/core/src/main/java/com/example/core/data/network/response/DataContainerResponse.kt
@@ -2,7 +2,7 @@ package com.example.core.data.network.response
 
 import com.google.gson.annotations.SerializedName
 
-data class DataContainer(
+data class DataContainerResponse(
     @SerializedName("results")
     val result: List<CharacterResponse>
 )
