32f76687fb8feff2a67d19248aabb1f52b022a90
derek-alves <derek.alves1998@hotmail.com>
2022-04-03 13:41:45 -0300

    step: add two new properties

--- curso-android-marvel-app/app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ curso-android-marvel-app/app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -3,6 +3,8 @@ package com.example.marvelapp.framework.network.response
 import com.google.gson.annotations.SerializedName
 
 data class DataContainerResponse(
+    val offset: Int,
+    val total:Int,
     @SerializedName("results")
     val result: List<CharacterResponse>
 )
