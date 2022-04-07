d368f90685f696fffcc91a9711471c3e0fcc8cfe
derek-alves <derek.alves1998@hotmail.com>
2022-04-07 16:47:55 -0300

    step: create first coroutines and change the threads wheres coroutines run

---
+++ courotines_study/app/src/main/java/com/example/courotines_study/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.courotines_study
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.courotines_study.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCount.setOnClickListener {
+            binding.tvCount.text = count++.toString()
+        }
+        binding.btnDownloadUserData.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private suspend  fun downloadUserData() {
+        withContext(Dispatchers.Main){
+            for (i in 1..200000) {
+                Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+                binding.tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+        }
+
+    }
+
+}
