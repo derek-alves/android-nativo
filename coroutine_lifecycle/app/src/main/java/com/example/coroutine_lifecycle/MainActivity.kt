eb6cb9813ed42988de69be4a4cfec9987dfe1f49
derek-alves <derek.alves1998@hotmail.com>
2022-04-15 00:14:02 -0300

    step: use live date to simplify and utilize life cycle automatic

--- coroutine_lifecycle/app/src/main/java/com/example/coroutine_lifecycle/MainActivity.kt
+++ coroutine_lifecycle/app/src/main/java/com/example/coroutine_lifecycle/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.coroutine_lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.coroutine_lifecycle.databinding.MainActivityBinding
 import com.example.coroutine_lifecycle.ui.main.MainFragment
+import com.example.coroutine_lifecycle.ui.main.MainViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,14 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+//        mainViewModel.getUsers() without live data needs to call
+        mainViewModel.users.observe(this, Observer{ myUsers ->
+            myUsers.forEach{
+                Log.i("myTag", "name is ${it.name}")
+            }
+        })
         lifecycleScope.launch {
             binding.progressBar.visibility = View.VISIBLE
             delay(1000)
--- coroutine_lifecycle/app/src/main/java/com/example/coroutine_lifecycle/ui/main/MainFragment.kt
+++ coroutine_lifecycle/app/src/main/java/com/example/coroutine_lifecycle/ui/main/MainFragment.kt
@@ -2,10 +2,12 @@ package com.example.coroutine_lifecycle.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.example.coroutine_lifecycle.R
 import kotlinx.coroutines.delay
@@ -28,9 +30,14 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        viewModel.getUsers()
+        viewModel.usersWithoutLiveData.observe(viewLifecycleOwner, Observer { myUsers ->
+            myUsers.forEach{
+                Log.i("myTag", "name is ${it.name}")
+            }
+        })
         lifecycleScope.launch { }
 
         //will launch, when the activity or fragment created for the first time
