58b524c04cfea241ea2b15bf7a8c80e6045d3ef4
derek-alves <derek.alves1998@hotmail.com>
2022-04-16 20:01:43 -0300

    step: implements usecase with flow and paging

--- curso-android-marvel-app/core/src/main/java/com/example/core/usecase/base/GetCharactersUseCase.kt
+++ curso-android-marvel-app/core/src/main/java/com/example/core/usecase/base/GetCharactersUseCase.kt
@@ -1,15 +1,20 @@
 package com.example.core.usecase.base
 
+import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import com.example.core.data.repository.CharactersRepository
 import com.example.core.domain.model.Character
 import kotlinx.coroutines.flow.Flow
 
 class GetCharactersUseCase(
+    private val charactersRepository: CharactersRepository
 ): PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
-        TODO("Not yet implemented")
+        return Pager(config = params.pagingConfig){
+            charactersRepository.getCharacters(params.query)
+        }.flow
     }
     data class GetCharactersParams(val query:String,val pagingConfig: PagingConfig)
 
