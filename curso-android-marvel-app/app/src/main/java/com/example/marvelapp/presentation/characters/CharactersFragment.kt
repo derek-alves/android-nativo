0f80aa2d70cbc6829496e511c539efab5f1baa5d
derek-alves <derek.alves1998@hotmail.com>
2022-04-19 15:19:39 -0300

    step: create lifecyleScope to get characts to show in fragment

--- curso-android-marvel-app/app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ curso-android-marvel-app/app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.core.domain.model.Character
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding:FragmentCharactersBinding get()= _binding!!
+
+    private val viewModel:CharactersViewModel by viewModels()
 
     private val charactersAdapter = CharactersAdapter()
 
@@ -30,13 +36,12 @@ class CharactersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initCharacterAdapter()
 
-        charactersAdapter.submitList(
-            listOf(
-                Character("3D-Ma", "https://avatars.githubusercontent.com/u/59737946?v=4"),
-                Character("3D-Ma", "https://avatars.githubusercontent.com/u/59737946?v=4"),
-                Character("3D-Ma", "https://avatars.githubusercontent.com/u/59737946?v=4")
-            )
-        )
+
+        lifecycleScope.launch {
+            viewModel.charactersPagingData("").collect { pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
     }
 
 
