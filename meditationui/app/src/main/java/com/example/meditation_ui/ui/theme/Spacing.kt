b408125b1d18f9d82fc2453b674f39f1d0cbc155
Derek Alves <dev.derek.alves@gmail.com>
2023-03-05 13:37:20 -0300

    step: add new size to spacing

--- meditationui/app/src/main/java/com/example/meditation_ui/ui/theme/Spacing.kt
+++ meditationui/app/src/main/java/com/example/meditation_ui/ui/theme/Spacing.kt
@@ -1,6 +1,5 @@
 package com.example.meditation_ui.ui.theme
 
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
@@ -8,16 +7,17 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-data class Spacing (
+data class Spacing(
     val default: Dp = 0.dp,
     val extraSmall: Dp = 4.dp,
     val small: Dp = 8.dp,
     val medium: Dp = 16.dp,
+    val extraMedium: Dp = 24.dp,
     val large: Dp = 32.dp,
     val extraLarge: Dp = 64.dp,
 )
 
-val LocalSpacing = staticCompositionLocalOf  { Spacing() }
+val LocalSpacing = staticCompositionLocalOf { Spacing() }
 
 val MaterialTheme.spacing: Spacing
     @Composable
