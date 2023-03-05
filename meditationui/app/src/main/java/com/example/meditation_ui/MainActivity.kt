db328d92e40b26fdb402a06282005c0fa1ccb2b7
Derek Alves <dev.derek.alves@gmail.com>
2023-03-05 13:36:53 -0300

    step: remove surface

--- meditationui/app/src/main/java/com/example/meditation_ui/MainActivity.kt
+++ meditationui/app/src/main/java/com/example/meditation_ui/MainActivity.kt
@@ -3,16 +3,7 @@ package com.example.meditation_ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.meditation_ui.ui.theme.MeditationuiTheme
-import com.example.meditation_ui.ui.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +11,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MeditationuiTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(MaterialTheme.spacing.small),
-                    color = MaterialTheme.colors.background
-                ) {
-                    HomeScreen()
-                }
+
+                HomeScreen()
+                
             }
         }
     }
