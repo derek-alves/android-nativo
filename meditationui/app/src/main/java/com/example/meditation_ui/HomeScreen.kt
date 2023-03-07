d081154abf8bf6db32acc407b419ce2a15fed1d2
Derek Alves <dev.derek.alves@gmail.com>
2023-03-06 22:09:02 -0300

    step: add components in home screen

--- meditationui/app/src/main/java/com/example/meditation_ui/HomeScreen.kt
+++ meditationui/app/src/main/java/com/example/meditation_ui/HomeScreen.kt
@@ -1,19 +1,92 @@
 package com.example.meditation_ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.meditation_ui.ui.theme.DeepBlue
-import com.example.meditation_ui.ui.theme.TextWhite
-import com.example.meditation_ui.ui.theme.spacing
+import androidx.compose.ui.unit.sp
+import com.example.meditation_ui.entity.Feature
+import com.example.meditation_ui.ui.components.ChipSection
+import com.example.meditation_ui.ui.components.CurrentMeditation
+import com.example.meditation_ui.ui.components.CustomTopAppBar
+import com.example.meditation_ui.ui.theme.*
+
+
+val features: List<Feature> = listOf(
+    Feature(
+        title = "Sleep meditation",
+        R.drawable.ic_headphone,
+        BlueViolet1,
+        BlueViolet2,
+        BlueViolet3
+    ),
+    Feature(
+        title = "Tips for sleeping",
+        R.drawable.ic_videocam,
+        LightGreen1,
+        LightGreen2,
+        LightGreen3
+    ),
+    Feature(
+        title = "Night island",
+        R.drawable.ic_headphone,
+        OrangeYellow1,
+        OrangeYellow2,
+        OrangeYellow3
+    ),
+    Feature(
+        title = "Calming sounds",
+        R.drawable.ic_videocam,
+        Beige1,
+        Beige2,
+        Beige3
+    ), Feature(
+        title = "Sleep meditation",
+        R.drawable.ic_headphone,
+        BlueViolet1,
+        BlueViolet2,
+        BlueViolet3
+    ),
+    Feature(
+        title = "Tips for sleeping",
+        R.drawable.ic_videocam,
+        LightGreen1,
+        LightGreen2,
+        LightGreen3
+    ),
+    Feature(
+        title = "Night island",
+        R.drawable.ic_headphone,
+        OrangeYellow1,
+        OrangeYellow2,
+        OrangeYellow3
+    ),
+    Feature(
+        title = "Calming sounds",
+        R.drawable.ic_headphone,
+        Beige1,
+        Beige2,
+        Beige3
+    )
+
+
+)
 
 @Preview(showBackground = true)
 @Composable
@@ -29,66 +102,90 @@ fun HomeScreen() {
 
 
 @Composable
-fun CustomTopAppBar(name: String = "Derek") {
-    TopAppBar(
-
-        backgroundColor = DeepBlue,
-        elevation = MaterialTheme.spacing.default,
-        title = {
-            Column() {
-
-                Text(text = buildAnnotatedString {
-                    withStyle(style = SpanStyle()) {
-                        append("Good morning, ")
-                    }
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                        append(name)
-                    }
-                }, style = MaterialTheme.typography.body1.copy(color = TextWhite))
-                Text(
-                    "We wish you have a good day!",
-                    style = MaterialTheme.typography.body2.copy(color = TextWhite)
-                )
-
-            }
-        }, actions = {
-
-            Row(modifier = Modifier.padding(horizontal = MaterialTheme.spacing.small)) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_search),
-                    contentDescription = "Search",
-                    tint = TextWhite,
-                    modifier = Modifier
-                        .size(MaterialTheme.spacing.extraMedium)
-
-                )
-            }
-
-
-        })
-
-
-}
-
-@Composable
 fun Body() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        ChipSection()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                MaterialTheme.sizes.medium
+            )
+    ) {
+        ChipSection(listOf("Android", "Flutter", "React native"))
+        CurrentMeditation()
+        FeatureSection(features = features)
     }
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ChipSection(chips: List<String>) {
-    var selectedChipIndex by remember {
-        mutableStateOf(0)
+fun FeatureSection(features: List<Feature>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            "Features",
+            style = MaterialTheme.typography.h6.copy(color = TextWhite),
+            modifier = Modifier.padding(
+                top = MaterialTheme.sizes.extraMedium,
+                bottom = MaterialTheme.sizes.medium
+            )
+        )
+        LazyVerticalGrid(cells = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.sizes.medium),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.sizes.medium),
+            content = {
+                items(features) { feature ->
+                    FeatureItem(feature)
+                }
+            })
     }
 
-    LazyRow {
-        items(count = chips.size) {
-            Box() {
+}
 
-            }
-        }
+@Composable
+fun FeatureItem(feature: Feature) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(MaterialTheme.sizes.small))
+            .background(feature.darkColor)
+            .padding(
+                horizontal =
+                MaterialTheme.sizes.medium,
+                vertical = MaterialTheme.sizes.extraMedium,
+            )
+    ) {
+
+
+        Text(
+            feature.title,
+            style = MaterialTheme.typography.h6,
+            color = Color.White,
+            lineHeight = 26.sp,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+        Icon(
+            painter = painterResource(id = feature.iconId),
+            contentDescription = feature.title,
+            tint = Color.White,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+        Text(
+            "Start",
+            color = TextWhite,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .clickable {
+                    // Handle the click
+                }
+                .align(Alignment.BottomEnd)
+                .clip(RoundedCornerShape(MaterialTheme.sizes.small))
+                .background(ButtonBlue)
+                .padding(
+                    vertical = MaterialTheme.sizes.small,
+                    horizontal = MaterialTheme.sizes.medium
+                )
+        )
+
     }
 }
