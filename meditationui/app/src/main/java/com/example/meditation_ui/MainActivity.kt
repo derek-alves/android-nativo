e9d56570ccf0b1d26e7dc14a994e00dbfc4fc714
Derek Alves <dev.derek.alves@gmail.com>
2023-03-07 19:42:41 -0300

    step: organize main activity

--- meditationui/app/src/main/java/com/example/meditation_ui/MainActivity.kt
+++ meditationui/app/src/main/java/com/example/meditation_ui/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.meditation_ui.ui.theme.MeditationuiTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationuiTheme {
-                HomeScreen()
+                MainScreen()
             }
         }
     }
 }
-
