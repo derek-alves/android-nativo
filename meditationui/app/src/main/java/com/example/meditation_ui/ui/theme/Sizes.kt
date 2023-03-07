df0012ab50fcf18e3922addf64d6ec46dba915de
Derek Alves <dev.derek.alves@gmail.com>
2023-03-06 22:10:23 -0300

    refactor: change spacing name to sizes

--- meditationui/app/src/main/java/com/example/meditation_ui/ui/theme/Sizes.kt
+++ meditationui/app/src/main/java/com/example/meditation_ui/ui/theme/Sizes.kt
@@ -7,19 +7,20 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-data class Spacing(
+data class Sizes(
     val default: Dp = 0.dp,
     val extraSmall: Dp = 4.dp,
     val small: Dp = 8.dp,
     val medium: Dp = 16.dp,
     val extraMedium: Dp = 24.dp,
     val large: Dp = 32.dp,
-    val extraLarge: Dp = 64.dp,
+    var extraLarge: Dp = 40.dp,
+    val extraXLarge: Dp = 64.dp,
 )
 
-val LocalSpacing = staticCompositionLocalOf { Spacing() }
+val LocalSizes = staticCompositionLocalOf { Sizes() }
 
-val MaterialTheme.spacing: Spacing
+val MaterialTheme.sizes: Sizes
     @Composable
     @ReadOnlyComposable
-    get() = LocalSpacing.current
+    get() = LocalSizes.current
