b3a8d0ba2ace75d45a3783af0783e7a44073a67d
Derek Alves <dev.derek.alves@gmail.com>
2023-03-10 15:55:07 -0300

    step: call viewModel instance

--- daggerhiltstudy/app/src/main/java/com/example/dagger_hilt_study/MainActivity.kt
+++ daggerhiltstudy/app/src/main/java/com/example/dagger_hilt_study/MainActivity.kt
@@ -10,14 +10,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.dagger_hilt_study.ui.theme.DaggerhiltstudyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DaggerhiltstudyTheme {
-
+                val viewModel = hiltViewModel<DaggerHiltStudyViewModel>()
             }
         }
     }
