551d2ebd964b2d9abcbd2def30e384420128732f
Derek Alves <dev.derek.alves@gmail.com>
2023-03-10 15:55:33 -0300

    step: get app context from params

--- daggerhiltstudy/app/src/main/java/com/example/dagger_hilt_study/data/repository/RepositoryImpl.kt
+++ daggerhiltstudy/app/src/main/java/com/example/dagger_hilt_study/data/repository/RepositoryImpl.kt
@@ -1,11 +1,18 @@
 package com.example.dagger_hilt_study.data.repository
 
+import android.app.Application
+import com.example.dagger_hilt_study.R
 import com.example.dagger_hilt_study.data.remote.Api
 import com.example.dagger_hilt_study.domain.repository.Repository
+import javax.inject.Inject
 
-class RepositoryImpl(private val api: Api): Repository{
+class RepositoryImpl @Inject constructor(private val api: Api, private val appContext: Application) : Repository {
+    init {
+        val appName = appContext.getString(R.string.app_name)
+        println("Hello from the repository. The app name is $appName")
+    }
     override suspend fun doNetWordCall() {
-        TODO("Not yet implemented")
+        api.doNetWorkCall()
     }
 
 }
