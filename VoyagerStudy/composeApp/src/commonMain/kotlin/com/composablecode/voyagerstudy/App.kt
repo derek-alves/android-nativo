60d989d1d156804aeedabc90bc5b27eb1760cc7b
Derek Alves <dev.derek.alves@gmail.com>
2024-12-02 23:56:12 -0300

    create base navigation

--- VoyagerStudy/composeApp/src/commonMain/kotlin/com/composablecode/voyagerstudy/App.kt
+++ VoyagerStudy/composeApp/src/commonMain/kotlin/com/composablecode/voyagerstudy/App.kt
@@ -10,28 +10,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
+import cafe.adriel.voyager.navigator.Navigator
+import com.composablecode.voyagerstudy.screens.ScreenA
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import voyagerstudy.composeapp.generated.resources.Res
 import voyagerstudy.composeapp.generated.resources.compose_multiplatform
 
+@OptIn(InternalVoyagerApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        Navigator(ScreenA())
     }
 }
---
+++ VoyagerStudy/composeApp/src/commonMain/kotlin/com/composablecode/voyagerstudy/screens/ScreenA.kt
@@ -0,0 +1,45 @@
+package com.composablecode.voyagerstudy.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class ScreenA :Screen{
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Screen A")
+            OutlinedTextField(
+                value ="Value" ,
+                onValueChange = {
+
+                },
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            )
+            Button(onClick = {
+                navigator.push(ScreenB("STANDOUT"))
+            }) {
+                Text("Navigate to Screen B")
+            }
+
+        }
+    }
+}
