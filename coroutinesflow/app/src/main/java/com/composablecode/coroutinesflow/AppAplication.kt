480d61f662980fd87aa91acb408b4c8773f2d529
Derek Alves <dev.derek.alves@gmail.com>
2024-12-20 16:16:40 -0300

    create applicationScope to make de execution of save tracking happens

---
+++ coroutinesflow/app/src/main/java/com/composablecode/coroutinesflow/AppAplication.kt
@@ -0,0 +1,11 @@
+package com.composablecode.coroutinesflow
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+class MyApplication: Application() {
+
+    val applicationScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+}
