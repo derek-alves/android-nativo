f95a8025aa562a03963d556e5f772627e5df1e81
Derek Alves <dev.derek.alves@gmail.com>
2024-12-27 01:49:38 -0300

    create base structure to backend database

--- VoyagerStudy/core/src/androidUnitTest/kotlin/com/composablecode/core/Test.android.kt
+++ VoyagerStudy/core/src/androidUnitTest/kotlin/com/composablecode/core/Test.android.kt
@@ -1,12 +1,10 @@
 package com.composablecode.core
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class AndroidGreetingTest {
 
     @Test
     fun testExample() {
-        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
     }
 }
