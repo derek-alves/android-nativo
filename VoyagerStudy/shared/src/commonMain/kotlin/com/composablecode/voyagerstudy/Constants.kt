2155c3daff33b3812c63471bf220b1fd04f96a56
Derek Alves <dev.derek.alves@gmail.com>
2025-01-17 00:07:27 -0300

    change the connection url and data base strucutre

--- VoyagerStudy/shared/src/commonMain/kotlin/com/composablecode/voyagerstudy/Constants.kt
+++ VoyagerStudy/shared/src/commonMain/kotlin/com/composablecode/voyagerstudy/Constants.kt
@@ -1,9 +1,9 @@
 package com.composablecode.voyagerstudy
 
 const val HTTP_HOST = "0.0.0.0"
-const val HTTP_PORT = 8080
+const val HTTP_PORT = 9090
 const val DB_NAME = "study"
-const val JDBC_URL = "jdbc:postgresql://db:5432/study"
+const val JDBC_URL = "jdbc:postgresql://localhost:5432/study"
 const val DRIVER_CLASS_NAME = "org.postgresql.Driver"
 const val DB_USER_NAME = "any-username"
 const val DB_USER_PASSWORD = "any-password"
